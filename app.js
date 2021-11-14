777577ab1617254ddccdb6c7411776752f060b96
Yizhe Zhan <yizhe.zhan@metservice.com>
2021-11-14 20:38:40 +1300

    initial commit

---
+++ app.js
@@ -0,0 +1,16 @@
+// 1. Common JS functions
+// console.log(), alert(), prompt()
+
+// 2. Variables in JS
+// = : assignment
+// let, const, var : variable declaration
+
+// let asdfasd = prompt('请输入姓名:')
+// alert('Hello ' + asdfasd)
+
+// 3. Numbers operators
+// 加法，减法，乘法，除法，remainder operator (%), power (**), ++, +=
+
+// 4. Strings
+// 文字String
+// 数字String
